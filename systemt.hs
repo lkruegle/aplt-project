743e19c00926a48fc7f0d57aa83e8d10d8fa4618
lkruegle <lukaskruegle@gmail.com>
2026-03-30 16:25:13 +0200

    Fix some issues with types, implement some of the evaluator

--- systemt.hs
+++ systemt.hs
@@ -1,17 +1,36 @@
 import qualified Data.Map as M
 
+data Nat = Zero | Succ Nat
+
 data Type = NatT | ArrT Type Type
 
 type Var = String
-data Value = Num Nat | Lam Var Type Exp
+data Value = VNat Nat | VLam Type Var Exp
 
 data Exp
-  = Var Var
+  = V Var
   | Z
   | S Exp
-  | Rec Exp Exp Exp
-  | Lam Type Var Exp
-  | Ap Exp Exp
+  | R Exp Exp Exp
+  | L Type Var Exp
+  | A Exp Exp
+
+-- prg = S $ S Z
+-- eval prg -- 2
+
+eval :: Env -> Exp -> Value
+eval env = \case
+  Z -> Zero
+  S e -> Succ $ eval env e
+  V name -> case M.lookup name env of
+    Just value -> value -- guaranteed by type checker
+  R n b r -> undefined
+  L t v e -> Lam t v e
+  A f a ->
+    let (Lam _ x e) = eval env f in
+    let v = eval env a in
+    let env' = Map.insert x v env in
+    eval env' e
 
 type Cxt = M.Map Var Type
 type Env = M.Map Var Value
@@ -21,6 +40,3 @@ infer = undefined
 
 check :: Cxt -> Exp -> Type -> Bool
 check = undefined
-
-eval :: Env -> Exp -> Value
-eval = undefined
