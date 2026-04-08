2068ec06c0b038c3c1ad7370c63991353765be6d
lkruegle <lukaskruegle@gmail.com>
2026-04-08 10:15:58 +0200

    Implement type checking for Lambda and Function app

--- src/TypeChecker.hs
+++ src/TypeChecker.hs
@@ -22,8 +22,19 @@ infer ctx (EVar id@(Ident n)) = case lookupCtx id ctx of
   Nothing -> Left $ "No variable bound with name " ++ n
   Just (Found typ idx) -> Right $ Inferred typ (Var idx)
 infer ctx (ERec e1 e2 e3 id e4) = undefined
-infer ctx (ELam id typ exp) = undefined
-infer ctx (EApl f a) = undefined
+infer ctx (ELam id typA exp) = case toSTyp typA of
+  SomeSTyp atyp -> do
+    Inferred rtyp fterm <- infer (ConsC id atyp ctx) exp
+    Right $ Inferred (SFun atyp rtyp) (Lam fterm)
+infer ctx (EApl f a) = do
+  Inferred ftyp fterm <- infer ctx f
+  case ftyp of
+    SFun atyp rtyp -> do
+      case check ctx atyp a of
+        Right aterm -> do
+          Right $ Inferred rtyp (App fterm aterm)
+        Left err -> Left $ "Function applied to incorrect argument type. " ++ err
+    _ -> Left "Function application applied to non-function term"
 infer ctx (ELet id vexp iexp) = do
   Inferred typ vterm <- infer ctx vexp
   Inferred typ' iterm <- infer (ConsC id typ ctx) iexp
