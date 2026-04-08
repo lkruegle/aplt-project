8ff7bdc1f84ee4e9ab9099c90ada953fc930d547
lkruegle <lukaskruegle@gmail.com>
2026-04-08 11:39:31 +0200

    Add natToInt function

--- src/Types.hs
+++ src/Types.hs
@@ -5,12 +5,16 @@ import Kx.Abs (Ident, Typ(..))
 data ℕ = Zero | Succ ℕ
   deriving (Eq, Show)
 
+natToInt :: ℕ -> Int
+natToInt Zero = 0
+natToInt (Succ n) = 1 + (natToInt n)
+
 data Val (τ :: Typ) where
   VNat :: ℕ -> Val TNat
   VFun :: (Val τ₁ -> Val τ₂) -> Val ('TFun τ₁ τ₂)
 
 instance Show (Val τ) where
-  show (VNat n) = show n
+  show (VNat n) = show $ natToInt n
   show (VFun _) = "λ"
 
 data (τ :: Typ) ∈ (γ :: [Typ]) where
@@ -35,22 +39,6 @@ consEnv :: Env γ -> Val τ -> Env (τ:γ)
 consEnv e x Here = x
 consEnv e x (There p) = e p
 
-data STyp (τ :: Typ) where
-  SNat :: STyp 'TNat
-  SFun :: STyp τ₁ -> STyp τ₂ -> STyp ('TFun τ₁ τ₂)
-
-data SomeSTyp where
-  SomeSTyp :: STyp τ -> SomeSTyp
-
-toSTyp :: Typ -> SomeSTyp
-toSTyp (TFun a r) = case (toSTyp a, toSTyp r) of
-  (SomeSTyp sa, SomeSTyp sr) -> SomeSTyp $ SFun sa sr
-toSTyp TNat = SomeSTyp SNat
-
-instance Show (STyp τ) where
-  show SNat = show TNat
-  show (SFun a b) = "(" ++ show a ++ " -> " ++ show b ++ ")"
-
 data Ctx (γ :: [Typ]) where
   EmptyC :: Ctx '[]
   ConsC :: Ident -> STyp τ -> Ctx γ -> Ctx (τ:γ)
@@ -68,3 +56,19 @@ lookupCtx n (ConsC n' typ ctx)
 
 data Inferred (γ :: [Typ]) where
   Inferred :: STyp τ -> γ ⊢ τ -> Inferred γ
+
+data STyp (τ :: Typ) where
+  SNat :: STyp 'TNat
+  SFun :: STyp τ₁ -> STyp τ₂ -> STyp ('TFun τ₁ τ₂)
+
+data SomeSTyp where
+  SomeSTyp :: STyp τ -> SomeSTyp
+
+toSTyp :: Typ -> SomeSTyp
+toSTyp (TFun a r) = case (toSTyp a, toSTyp r) of
+  (SomeSTyp sa, SomeSTyp sr) -> SomeSTyp $ SFun sa sr
+toSTyp TNat = SomeSTyp SNat
+
+instance Show (STyp τ) where
+  show SNat = show TNat
+  show (SFun a b) = "(" ++ show a ++ " -> " ++ show b ++ ")"
