9a35651bce00658f5f65220876475817f4f5f9f4
lkruegle <lukaskruegle@gmail.com>
2026-04-29 21:04:13 +0200

    Implement stepwise evaluation

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -3,18 +3,27 @@ module Evaluator where
 import Types
 import BookKeeping
 
-evaluate :: Exp -> Either String Val
-evaluate = eval
-
-eval :: Exp -> Either String Val
-eval e = case toVal e of
-  Just val -> Right val
-  Nothing -> step e >>= eval
-
-step :: Exp -> Either String Exp
-step = _
+evaluate :: Exp -> Val
+evaluate e = case toVal e of
+  Just val -> val
+  Nothing -> evaluate . step $ e
 
 toVal :: Exp -> Maybe Val
+-- 16.3a
 toVal (EFLam t e) = Just $ VLam t e
+-- 16.3b
 toVal (ETLam e) = Just $ VTLam e
 toVal _ = Nothing
+
+-- LAZY EVALUATION STRAT
+step :: Exp -> Exp
+-- 16.3c LAZY ONLY
+step (EFApp (EFLam _ body) arg) = substExp arg body
+-- 16.3d
+step (EFApp e arg) = EFApp (step e) arg
+-- 16.3f
+step (ETApp (ETLam body) t) = substTypInExp t body
+-- 16.3g
+step (ETApp e t) = ETApp (step e) t
+step e =
+  error $ "Given expression " <> show e <> " has no valid step-wise dynamics."
