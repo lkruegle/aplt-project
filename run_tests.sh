0b99142a5b7122b16c2a33b9ade50186a9a8294b
lkruegle <lukaskruegle@gmail.com>
2026-05-11 10:44:49 +0200

    Update test script

--- run_tests.sh
+++ run_tests.sh
@@ -2,14 +2,17 @@
 
 BLUE="\033[0;34m"
 GREEN="\033[0;32m"
+RED="\033[0;31m"
 NC="\033[0m"
 
-set -e
 echo -e "${GREEN}==> Running make...${NC}"
 make
 echo -e "\n${GREEN}==> Running tests...${NC}"
 
+failures=0
 for f in test/*; do
   echo -e "\n${BLUE}--- $f ---${NC}"
-  ./line "$f"
+  ./line "$f" || { echo -e "${RED}==> exited with $?${NC}"; ((failures++)); }
 done
+
+echo -e "\n${GREEN}==> Done. $failures failure(s).${NC}"
